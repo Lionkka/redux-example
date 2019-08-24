e2beee21bd629bf582f90aff599e43a9a155b192
Alona <bestliona@gmail.com>
2019-08-24 12:58:15 +0300

    fix redux

--- src/componets/List/List.js
+++ src/componets/List/List.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function List (props) {
+    console.log('props', props)
     return <ul>
         {props.todos.map(({title, id})=> <li key={id}>{title}</li>)}
     </ul>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import {combineReducers} from 'redux'
-import { connectRouter } from 'connected-react-router'
 import todos from './todos'
 import filter from './filter'
 
-export default (history)=> combineReducers(
+export default combineReducers(
     {
-        router: connectRouter(history),
         todos,
         filter
     }
